b822c40fc06f76e1d7c1629c06a908691bd28580
asiagovincent11@gmail.com <asiagovincent11@gmail.com>
2023-10-19 10:20:30 +0300

    done with the lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+const taskInput = document.getElementById('new-task-description');
+const form = document.getElementById('create-task-form');
+const taskList = document.getElementById('tasks');
+
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  const taskDescription = taskInput.value; 
+
+  
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `
+    <input type="checkbox">
+    <span>${taskDescription}</span>
+  `;
+
+  
+  taskList.appendChild(listItem);
+
+  taskInput.value = ''; 
+});
